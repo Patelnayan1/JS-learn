2413ff3169a08da8681dd875e7259b68731ef1bc
Patel Nayan <64275257+Patelnayan1@users.noreply.github.com>
2024-03-20 05:11:19 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =  144553
+let accountEmail = "nayan@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 // not allowed 
+
+accountEmail = "hc@hc.com"
+accountPassword = "786786"
+accountCity = "ahme"
+
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState] )
